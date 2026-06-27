1366ec9c9c378974b1914176250e1ce331fb21e2
ilham <ilham@code.id>
2026-06-27 13:51:41 +0700

    add netlify.toml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  ssr: false,
   css: [
     '~/assets/css/main.css'
   ],
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      crawlLinks: true
+    }
+  },
   modules: ['@pinia/nuxt', '@nuxt/ui'],
   pinia: {
     storesDirs: ['./app/stores/**']
